95b4a7672f668b72222418893745587045ab0a71
Sorebit <cmbk90@gmail.com>
2020-06-11 18:29:24 +0200

    Move files to new dir

---
+++ B/b2.c
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "inc/hw_memmap.h"
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+int
+main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+
+    // Ustawienie pinu PG2
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOG);
+    GPIOPinTypeGPIOOutput(GPIO_PORTG_BASE, GPIO_PIN_2);
+
+    //GPIOPinWrite(GPIO_PORTG_BASE, GPIO_PIN_2, 0x00); // Zgaszenie diody
+    GPIOPinWrite(GPIO_PORTG_BASE, GPIO_PIN_2, 0x04); // Zapalenie diody
+    while(1) {}
+}
